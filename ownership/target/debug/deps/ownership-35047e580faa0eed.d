f4ac0ea7f445e128ca44fdbbd77bfbf248ad6027
dwes7 <dwestley.1992@gmail.com>
2022-11-05 01:00:11 +0000

    removing build files

--- ownership/target/debug/deps/ownership-35047e580faa0eed.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust-tutorial/ownership/target/debug/deps/ownership-35047e580faa0eed: src/main.rs
-
-/workspaces/rust-tutorial/ownership/target/debug/deps/ownership-35047e580faa0eed.d: src/main.rs
-
-src/main.rs:
--- ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust-tutorial/ownership/target/debug/ownership: /workspaces/rust-tutorial/ownership/src/main.rs
